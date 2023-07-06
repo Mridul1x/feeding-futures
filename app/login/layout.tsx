d728bd8e62e6cccc327c77d4df411f928a327fcb
Mridul1x <mridulhasannn@gmail.com>
2023-07-07 04:17:26 +0600

    Login and Register Page Added

---
+++ app/login/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Feeding Futures | Login",
+};
+
+export default function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
---
+++ app/register/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Feeding Futures | Register",
+};
+
+export default function RegisterLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
