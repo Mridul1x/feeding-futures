9ef5d6c6f1b495f5b8736d710c53db30d36dba2b
Mridul1x <mridulhasannn@gmail.com>
2023-07-07 04:20:10 +0600

    User Dashboard added

---
+++ app/dashboard/user/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Feeding Futures | User Dashboard",
+};
+
+export default function UserLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
