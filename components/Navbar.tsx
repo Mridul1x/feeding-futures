667f528492957aa41f4e0e7209df4fcdee47d3b2
Mridul1x <mridulhasannn@gmail.com>
2023-07-11 23:15:59 +0600

    Bug Fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -95,7 +95,7 @@ const Navbar = () => {
           </ul>
         </div>
         <Link href="/" className="btn btn-ghost normal-case text-xl">
-          elysianFeeds
+          Feeding Futures
         </Link>
       </nav>
 
