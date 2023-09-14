78ab19adb6ff46d38a861262cbfa536fc2234566
Mridul1x <mridulhasannn@gmail.com>
2023-09-14 16:32:48 +0600

    Fixed

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -12,7 +12,7 @@ const SectionTitle: React.FC<SectionTitleProps> = ({ title }) => {
       <motion.h2
         initial={{ y: "-100%" }}
         whileInView={{ y: 0 }}
-        transition={{ ease: "easeInOut", duration: 0.75 }}
+        transition={{ ease: "easeInOut", duration: 0.60 }}
         className="text-2xl md:text-4xl font-semibold text-white text-center uppercase mb-10"
       >
         {title}
